c3985fdcb9b45671e4685f20d02de7050dc5ecce
Landon GB <landogbland@gmail.com>
2012-12-23 15:42:20 -0700

    Initial commit

---
+++ landon_away_log.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2008-2012  See the AUTHORS file for details.
+ * Copyright (C) 2006-2007, CNU <bshalm@broadpark.no> (http://cnu.dieplz.net/znc)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ */
+
+#include <znc/FileUtils.h>
+#include <znc/User.h>
+#include <znc/IRCNetwork.h>
+#include <znc/Chan.h>
+#include <znc/Server.h>
+
+class LandonAwayLog: public CModule {
+public:
+    MODCONSTRUCTOR(LandonAwayLog) {}
+
+    /* Methods for setting the away status to away or here */
+    void Away();
+    void Here();
+
+    // Module first load
+    virtual bool OnLoad(const CString& sArgs, CString& sMessage);
+
+    // On PM handler
+    virtual EModRet OnPrivMsg(CNick& Nick, CString& sMessage);
+
+    // When a client lgos in, send them any saved messages we have
+    virtual void CModule::OnClientLogin();
+
+private:
+    bool away;                  // Flag for if you are away
+    std::vector savedMessages;  // Saved messages
+};
+
+
+void LandonAwayLog::SetAwayStatus(bool status) {
+    away = status;
+    if(away == false)
+        savedMessages.clear();
+}
+
+bool LandonAwayLog::OnLoad(const CString& sArgs, CString& sMessage) {
+    away = false;
+}
+
+void LandonAwayLog::OnClientLogin() {
+    if(away == true) {
+        for (unsigned i=0; i<savedMessages.size(); i++) {
+            broadcast(savedMessages.at(i));
+        }
+    }
+}
+
+CModule::EModRet LandonAwayLog::OnPrivMsg(CNick& Nick, CString& sMessage) {
+    // If the pm comes from this log, ignore it so we don't get compound away messages
+    if(Nick.GetNick() != 'landon_away_message') {
+        savedMessages.put("<" + Nick.GetNick() + "> " + sMessage);
+    }
+    return CONTINUE;
+}
+
+template<> void TModInfo<LandonAwayLog>(CModInfo& Info) {
+    Info.AddType(CModInfo::NetworkModule);
+    Info.AddType(CModInfo::GlobalModule);
+    Info.SetHasArgs(false);
+}
+
+USERMODULEDEFS(LandonAwayLog, "Save PM's while marked as away")
